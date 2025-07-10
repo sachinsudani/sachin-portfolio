1f8b76d4c08f8347724fac2df3595c729a188ef6
Sachin Sudani <sachin@xenett.com>
2025-07-10 14:16:59 +0530

    feat: Add Newsletter component for subscription updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Blogs from "./components/Blogs";
 import Hero from "./components/Hero";
+import Newsletter from "./components/Newsletter";
 import Projects from "./components/Projects";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <Projects />
       <Blogs />
+      <Newsletter />
     </>
   );
 }
