e8d1f0ba2f3a074756d87ed80b5bb2f6b5b70543
Sachin Sudani <sachin@xenett.com>
2025-07-10 13:49:04 +0530

    feat: Add Blogs component to display latest blog posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Blogs from "./components/Blogs";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 
@@ -6,6 +7,7 @@ export default function Home() {
     <>
       <Hero />
       <Projects />
+      <Blogs />
     </>
   );
 }
